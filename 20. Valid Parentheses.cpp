139b4e07d73f4d99cbf46604469911e14b410078
gillwei <91458475+gillwei@users.noreply.github.com>
2024-01-25 17:56:08 +0800

    Modify 20. Valid Parenthes
    string中是open bracket搭配 close packet
    因為有先後順序的概念所以要用stack
    stack 最開始必須是open, 加入stack
    如果是close 的話, stack 最前面必須是對應的open bracket
    如果是的話就把open那個pop出來, 消除掉

--- 20. Valid Parentheses.cpp
+++ 20. Valid Parentheses.cpp
@@ -1,50 +1,45 @@
 class Solution {
 public:
     bool isValid(string s) {
-    //創一個stack 一個一個儲存前括號, 然後如果有後括號就把對應的前括號消掉
-    //最後只要stack 是空的就對了, 否則就是錯
-    //不能用unordered_map, 因為有穿插的前括號和後括號的話, 還是算不對
         stack<char> sc;
-        int i = 0;
-        while(i<s.size())
+        for(int i=0; i < s.length(); i++)
         {
-            //如果遇到前括號就放進去 
-            if(s[i]=='(' || s[i]=='{' || s[i]=='[') sc.push(s[i]);
-	    // 如果遇到後刮號就要檢查現在最上面是不是對應的前括號, 否則順序就不對了
-	    // 例如: "({)}" 這個就算不對
-            if(s[i]==')')
+            // stack 最開始必須是open, 加入stack 
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[' )
             {
-	    	// 這邊要檢查如果一開始就遇到後括號就不對了
-                if(sc.empty() == true)
-                    return false;
-                if(sc.top() == '(')
-                    sc.pop();
-                else
-                    return false;
+                sc.push(s[i]);
             }
-            if(s[i]=='}')
+            else
             {
-                if(sc.empty() == true)
-                    return false;
-                if(sc.top() == '{')
-                    sc.pop();
+                // 不是open bracket
+                // 若不是open bracket 但stack 裡面又不是空的, return false
+                if(!sc.empty())
+                {
+                    // 如果是close 的話, stack 最前面必須是對應的open bracket
+                    // 如果是的話就把open那個pop出來, 消除掉
+                    if((s[i] == ')' && sc.top() == '(') || (s[i] == ']' && sc.top() == '[') || (s[i] == '}' && sc.top() == '{'))
+                    {
+                        sc.pop();
+                    }
+                    // 不是open bracket 又沒辦法close掉, return false
+                    else
+                    {
+                        return false;
+                    }
+                }
                 else
+                {
                     return false;
+                }
             }
-            if(s[i]==']')
-            {
-                if(sc.empty() == true)
-                    return false;
-                if(sc.top() == '[')
-                    sc.pop();
-                else
-                    return false;
-            }
-            i++;
         }
         if(sc.empty() == true)
+        {
             return true;
+        }
         else
+        {
             return false;
+        }
     }
 };
