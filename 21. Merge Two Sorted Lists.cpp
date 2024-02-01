4bba9b259094bb68d3b7a27e479651ad285fff1f
gillwei <91458475+gillwei@users.noreply.github.com>
2024-02-01 17:22:07 +0800

    21. Merge Two Sorted Lists
    把兩個已經sorting過的list 結合起來
    比較兩個link list 個別的element大小, 然後填入比較小的值到新的list中
    
    用雙指標指針, p1 對應 list1, p2 對應list2
    要先創造一個有值的 dummy, 做為head 紀錄保存下來
    然後p 本身會跟著list1的p1或 list2的p2
    
    比較p1和p2大小, 把比較小的節點接到p指標
    這裡不能用p->val = p2->val; 因為當p = p->next之後, p就會變成nullptr, compiler不會過
    所以要把p1或p2給 p-> next

---
+++ 21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // 要先創造一個有值的 dummy, 做為head 紀錄保存下來
+        // 然後p 本身會跟著list1的p1或 list2的p2
+        ListNode *dummy = new ListNode(-1);
+        ListNode *p = dummy;
+        ListNode *p1 = list1;
+        ListNode *p2 = list2;
+        while(p1 != nullptr && p2 != nullptr)
+        {
+            // 比較p1和p2大小, 把比較小的節點接到p指標
+            // 這裡不能用p->val = p2->val; 因為當p = p->next之後, p就會變成nullptr, compiler不會過
+            // 所以要把p1或p2給 p-> next 
+            if(p1->val > p2->val)
+            {
+                p->next = p2;
+                p2 = p2->next;
+            }
+            else
+            {
+                p->next = p1;
+                p1 = p1->next;
+            }
+            // p指標本身不斷前進
+            p = p->next;
+        }
+        if( p1 != nullptr )
+        {
+            p->next = p1;
+        }
+        if( p2 != nullptr )
+        {
+            p->next = p2;
+        }
+        return dummy->next;
+
+    }
+};
