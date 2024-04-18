a201bf7deb0522331f3c3240c9145c0d086a1f7d
gillwei <91458475+gillwei@users.noreply.github.com>
2024-04-18 11:04:58 +0800

    206. Reverse Linked List
    題意: 將一個 Linked List 反轉, 也就是把原本的 head 變成 tail, 然後
    tail->next 為 nullptr
    記得題目要看到最後一行!!  題目有寫說可以用 iterative / recursive,
    所以最好是用這兩種方式
    
    看 Neetcode 的解說
    https://www.youtube.com/watch?v=G0_I-ZF0S38
    Iterative 的方式比較好理解, 每次將目前的 Node 做為 current node,
    然後訂一個 previous node, current node 的 next 要改為previous node,
    然後每次迭代 current node 和 previous node 都要往前一格
    等到最後 current node 為 nullptr 的時候, previous node 就會是最前面的
    head
    
    Recursive 的方式: 只要提到 Recursive 就要記得要處理 sub-problem,
    所以每次要處理的 Linked list 必定是之後的, 例如 1->2->3->
    目標就會是要處理 1 必須先處理 2->3, 要處理 2->3 就先處理 3,
    然後有個技巧是反轉 linked list 可以用 head->next->next = head;

---
+++ 206. Reverse Linked List.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    // Iterative method 
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while(curr != nullptr) {
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+    
+    // Recursive method 
+    // ListNode* reverseList(ListNode* head) {
+    //     if(head == NULL || head->next == NULL) return head;
+    //     ListNode* newHead = reverseList(head->next);
+    //     head->next->next = head;
+    //     head->next=NULL;
+    //     return h2;
+    // }
+};
