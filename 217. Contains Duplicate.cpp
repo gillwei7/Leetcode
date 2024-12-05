b52807e3dfbed8b76b572c2394ac816a5a34c0fc
gillwei7 <gillwei7@gmail.com>
2024-12-06 07:22:20 +0800

    Improve "217.Contains Duplicate"
    Modify contains number function from hash map to hash set
    Can use lower memory

--- 217. Contains Duplicate.cpp
+++ 217. Contains Duplicate.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map <int, int> nums_storage;
+        // 20241206
+        // 使用 unordered hash set, 檢查每一個新增元素, 如果有重複的整個 function 就判定為真
+        // 如果沒檢查到重覆的就加進去 hash set, 直到整個 vector 都檢查完, 沒有重複的便是 false 
+
+        /*這邊 hash map 可以用 hash set 代替 */
+        //unordered_map <int, int> nums_storage;
+        unordered_set <int> nums_storage;
         for(int i = 0;i < nums.size(); i++) {
             if(nums_storage.find(nums[i]) == nums_storage.end()) {
-                nums_storage[nums[i]]++;
+                //nums_storage[nums[i]]++; // 用 hash set 代替
+                nums_storage.insert(nums[i]);
             }
             else{
                 return true;
