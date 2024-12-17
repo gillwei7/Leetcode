e8f05b5a944055ae01dbe457535e84c3cd8d84c4
Gill Wei (魏鴻憲) <Gill.Wei@quantatw.com>
2024-12-17 09:56:18 +0800

    226. Invert Binary Tree
    題意: 經典的 Swap Binary tree 題目, 把所有的左子樹和右子樹做交換
    
    要小心, 在這個情況下的交換左右子樹, 要寫在整個 function 裡面,
    不能分成另外一個 swapNode, 不然就會只有交換到最左邊那排和最右邊那排
    最前面記得要檢查是否為 nullptr, 是的話就 return

---
+++ 226. Invert Binary Tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        // 這邊處理的除了原本 root 是空的, 還有就是最後的葉子樹 case null 的話
+        // 一定要return
+        if(root==nullptr)
+            return nullptr;
+        // Swap function 要寫在裡面, 如果寫在外面另一個 function , 然後再帶入的話
+        // 就只有 swap 到最左邊和最右邊的一排
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        // 處理完左邊和右邊的子樹就可以 return 了
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;
+    }
+
+};
