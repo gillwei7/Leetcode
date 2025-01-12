afba8125161446d34538ccd260e961fdd72b6d2a
Gill Wei (魏鴻憲) <Gill.Wei@quantatw.com>
2025-01-12 10:31:07 +0800

    136. Single Number.cpp 暴力解
    題意: 輸入一串整數列, 裡面除了一個整數只有一個, 其他都是兩個,
    輸出只有一個的那個整數
    
    解法:
    暴力解, 把整數數列的每個數字都收到 hash map, 這裡有兩種方式,
    一種是無腦一直收, 收完之後再檢查哪一個 key 對應到的數字為 1
    另一種是有收到重複的數字就把 hash map 裡面的刪掉, 但是就算這樣做
    memory complexity 也還是 O(n),
    因為有可能都沒重複的一直收到後半段才找到重複的, 這種情況下是 O(n/2)
    簡化以後還是 O(n)

---
+++ 136. Single Number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        // 使用 hashmap, 計算每個整數出現的次數
+        // 最後再算出只有出現 1 次的那個整數
+        // 這樣需要存整個 array 所以需要 O(n)
+        // 另一個方式是可以只要有找到重複的就可以把原有存的整數刪掉
+        // 但是因為最糟的 case 有可能我們需要存 n/2 的個數,
+        // 這些整數最後才被取出來, 所以還是需要 O(n)
+        unordered_map <int,int> countNum;
+        int index = 0;
+         while(index < nums.size()) {
+            countNum[nums[index]]++;
+            index++;
+         }
+        for (auto it = countNum.begin(); it != countNum.end(); ++it) {
+            int number = it->first;  // Get the key (number)
+            int count = it->second; // Get the value (count)
+
+            if (count == 1) {
+                std::cout << "Number: " << number << "\n";
+                return number;
+            }
+        }
+        return 0;
+    }
+};
