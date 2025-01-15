a8492c213ef31c2e77de9c7d944dd48979d3db04
Gill Wei (魏鴻憲) <Gill.Wei@quantatw.com>
2025-01-16 07:02:16 +0800

    268. Missing Number.cpp
    題意: 給定一個 n 個數字的陣列, 找出 0 ~ n 這個陣列裡面沒有的數字
    
    解法:
    暴力解, 不考慮 memory 的話,
    首先創立一個 hash map 把有遇到的數字加進去
    檢查從 0 到 n 之中, 在 hashMap 找不到的數字
    這裡如果是用直接找 hashMap 之中為 false 的數字, 會找不到
    因為從來沒有被加入過
    所以必須用 find() == end() 來尋找裡面沒有的
    
    語法
    if (hashMap.find(i) == hashMap.end())
    表示 i 在 hashMap 中找不到這個 key

---
+++ 268. Missing Number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int result;
+        // 這裡把有遇到的數字加進去,
+        unordered_map<int,bool> hashMap;
+        for(int i = 0;i < n;i++) {
+            hashMap[nums[i]] = true;
+        }
+        
+	// 檢查從 0 到 n 之中, 在 hashMap 找不到的數字
+	// 這裡如果是用直接找 hashMap 之中為 false 的數字, 會找不到
+	// 因為從來沒有被加入過
+	// 所以必須用 find() == end() 來尋找裡面沒有的
+	
+        for (int i = 0; i <= n; ++i) {
+            if (hashMap.find(i) == hashMap.end()) {
+                return i; // Missing number
+            }
+        }
+        return -1;
+
+    }
+};
