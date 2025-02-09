a0924ac9993f6711f6572a2bbef05ed1bd3c59a5
gillwei7 <gillwei7@gmail.com>
2025-02-10 07:25:04 +0800

    9. Palindrome Number.cpp memory complexity O(1)
    前面的檢查除了檢查 x 是否為負數以外, 還要看尾數是否為 0
    如果是 0 的話一定不符合條件
    
    優化 memory complexity, 可以只要記一個從尾巴倒數過來的數字, 然後一邊
    把 x / 10, 最後比較這兩個數字
    
    如果十位數總共有奇數個, 例如 12331, 最後 reverseHalf 會變 133
    也就是從後面數來共三個, x 會變 12, 所以檢查 reverseHalf / 10 跟 x 是否相同
    如果十位數總共有偶數個, 例如 1231, 最後 reverseHalf 會變 13, x 會變 12
    檢查兩者是否相同即可

--- 9. Palindrome Number.cpp
+++ 9. Palindrome Number.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // 如果 x 是負的, 就像例子裡面提到的, 一定不符合條件
+    #if 0
+        // 首先如果 x 是負的, 就像例子裡面提到的, 一定不符合條件
         // 另外如果 x 只有一個數字也可以分開處理, 因為一定符合條件
         if(x<0) {
             return false;
@@ -24,5 +25,24 @@ public:
             }
         }
         return true;
+    #else
+        // 首先如果 x 是負的, 就像例子裡面提到的, 一定不符合條件
+        // 另外如果 x 尾數是 0, 除了 0 本身符合條件, 其他都會造成實際少一個數字
+        // 例如 9890, 倒過來是 0989 一定不符合
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+        return false;
+         }
+
+        int reverseNum = 0;
+        while(x > reverseNum){
+            reverseNum = reverseNum*10 + x%10;
+            x = x/10;
+        }
+        // 如果十位數總共有奇數個, 例如 12331, 最後 reverseNum 會變 133
+        // 也就是從後面數來共三個, x 會變 12, 所以檢查 reverseNum / 10 跟 x 是否相同
+        // 如果十位數總共有偶數個, 例如 1231, 最後 reverseNum 會變 13, x 會變 12
+        // 檢查兩者是否相同即可
+        return (reverseNum/10 == x || reverseNum == x);
+    #endif
     }
 };
