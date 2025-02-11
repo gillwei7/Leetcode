25c68f7b895d991d623030f50d03b34904ed90db
gillwei7 <gillwei7@gmail.com>
2025-02-12 06:59:38 +0800

    更新 121. Best Time to Buy and Sell Stock.cpp

--- 121. Best Time to Buy and Sell Stock.cpp
+++ 121. Best Time to Buy and Sell Stock.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        // 可能會有只有零個或是只有一個的輸入, 先處理掉
+        if(prices.size() == 0 || prices.size() == 1){
+            return 0;
+        }
         int l = 0;
         int r = 1;
-        int maxProfit = 0;
-        int currentProfit = 0;
-        while(r < prices.size()) {
-            if(prices[l] < prices[r]) {
-                currentProfit = prices[r] - prices[l];
-                maxProfit = max(maxProfit, currentProfit);
+        int result = prices[1]-prices[0]; // 這邊不能用 prices[r] - prices[l]
+        // 以右指標為基準一直往右數, 
+        // 如果新的右指標比原本左指標小, 就把左指標置換掉
+        // 如果新的右指標減左指標計算起來會比原本的結果大, 就置換掉原來的結果
+        while(r < prices.size()){
+            if(prices[r] < prices[l]) {
+                l = r;
+            } else if((prices[r] - prices[l]) > result){
+                result = prices[r]-prices[l];
             }
-            else {
-                l  = r;
-            }
-            r++;   
+            r++;
         }
-        return maxProfit;
+        // 結果有可能是負的, 要過濾這種情況
+        if(result > -1){
+            return result;
+        } else {
+            return 0;
+        }
+
     }
 };
