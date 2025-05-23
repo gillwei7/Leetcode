3c48f119249bf1954d50a37328cd377a6c1acc0d
gillwei7 <gillwei7@gmail.com>
2025-05-24 07:07:07 +0800

    70. Climbing Stairs.cpp 2nd
    Dynamic programming
    直接使用從 f(0),f(1),f(2) 開始記錄起來
    然後後面就可以直接使用前面兩個數字相加即可
    要注意因為 vector 的 index 會從 0 開始存, 所以如果 init 的時候用 vector<int> dp(n)
    會發現最後存取到沒有初始化的 dp[n], 所以記得要初始化 vector<int> dp(n+1)
    但是這個方法還是會用到 memory n
    
    所以我們只要把要用到的前兩個 prev1 和 prev2 存起來就好了
    有新的進來就把舊的換掉, 不過還會需要一個 current 當作 temp value

--- 70. Climbing Stairs.cpp
+++ 70. Climbing Stairs.cpp
@@ -13,19 +13,47 @@ public:
     // }
 
     // Dynamic programming
-    int solve(int n, vector<int> &dp) {
-        if(n <= 2)
-            return n;
-        if(dp[n] != -1)
-            return dp[n];
-        dp[n] = solve(n-1, dp) + solve(n-2, dp);
-        return dp[n];
-    }
+    // 直接使用從 f(0),f(1),f(2) 開始記錄起來
+    // 然後後面就可以直接使用前面兩個數字相加即可
+    // 要注意因為 vector 的 index 會從 0 開始存, 所以如果 init 的時候用 vector<int> dp(n)
+    // 會發現最後存取到沒有初始化的 dp[n], 所以記得要初始化 vector<int> dp(n+1)
+    // 但是這個方法還是會用到 memory n
+    
+    // int climbStairs(int n) {
+    //     if(n == 1){
+    //         return 1;
+    //     } else if (n == 2){
+    //         return 2;
+    //     }
 
+    //     vector<int> dp(n+1);
+    //     dp[0] = 1;
+    //     dp[1] = 1;
+    //     dp[2] = 2;
+    //     for(int i = 3;i <= n;i++){
+    //         dp[i] = dp[i-1] + dp[i-2];
+    //     }
+    //     return dp[n];
+    // }
+
+    // 所以我們只要把要用到的前兩個存起來就好了
+    // 有新的就把舊的換掉, 不過還需要第三個變數當作 temp value
     int climbStairs(int n) {
-        if(n <= 2)
-            return n;
-        vector<int> dp(n+1, -1); // 這邊要存 n+1 個元素, 因為要涵蓋到n=0的範圍
-        return solve(n,dp);
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+
+        int prev1 = 2; // Ways to climb 2 steps
+        int prev2 = 1; // Ways to climb 1 step
+        int current;
+        for (int i = 3; i <= n; i++) {
+            current = prev1 + prev2; // Ways to climb i steps
+            prev2 = prev1;           // Update prev2 to prev1
+            prev1 = current;         // Update prev1 to current
+        }
+        return current;
     }
 };
