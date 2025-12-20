e66be239e256c6406c499c4a7e3abb90bd0a11c2
gillwei7 <gillwei7@gmail.com>
2025-12-20 15:54:07 +0800

    2451. Odd String Difference.cpp optimize
    程式可以再優化, 只要比較到第三個就知道 words[0] 是不是 odd string
    只要 words[0] 不是 odd string, 那麼只要比較到下一個 string 跟 words[0]
    不一樣的情況, 那個 string 就一定是 odd string

--- 2451. Odd String Difference.cpp
+++ 2451. Odd String Difference.cpp
@@ -30,4 +30,44 @@ public:
        
         return result;
     }
+    // 程式可以再優化, 只要比較到第三個就知道 words[0] 是不是 odd string
+    // 只要 words[0] 不是 odd string, 那麼只要比較到下一個 string 跟 words[0]
+    // 不一樣的情況, 那個 string 就一定是 odd string
+    #if 0
+    string oddString(vector<string>& words) {
+        int n = words[0].size();
+
+        // Helper function to compare difference arrays of two strings
+        auto sameDiff = [&](const string& a, const string& b) {
+            for (int i = 0; i + 1 < n; i++) {
+                if (a[i + 1] - a[i] != b[i + 1] - b[i]) {
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        // Compare difference patterns among first three strings
+        bool d01 = sameDiff(words[0], words[1]);
+        bool d02 = sameDiff(words[0], words[2]);
+
+        // If words[0] differs from both, it must be the odd string
+        if (!d01 && !d02) {
+            return words[0];
+        }
+
+        // Otherwise, determine the correct (common) pattern
+        string& reference = d01 ? words[0] : words[1];
+
+        // Find the odd string by comparing with the reference
+        for (int i = 0; i < words.size(); i++) {
+            if (!sameDiff(reference, words[i])) {
+                return words[i];
+            }
+        }
+
+        // Guaranteed to find one by problem statement
+        return "";
+    }
+    #endif
 };
